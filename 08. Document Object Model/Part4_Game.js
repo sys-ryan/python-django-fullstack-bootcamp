90d74f7452e705d3cf3eee6a56245f588eef7180
Ryan Seong-Jin Kim <63445753+sys-ryan@users.noreply.github.com>
2020-12-03 15:37:15 +0900

    Update 08.Document Object Model

--- 08. Document Object Model/Part4_Game.js
+++ 08. Document Object Model/Part4_Game.js
@@ -2,17 +2,34 @@ console.log('connected')
 
 
 // Restart btn
-var restart = document.textContent('#b')
+var restart = document.querySelector('#b')
 
 //Grabs all the squares
 var squares = document.querySelectorAll('td');
 
 //Clear all the squares
 function clearBoard() {
-    false(var)
+    for(var i=0; i<squares.length; i++){
+        squares[i].textContent = '';
+    }
 }
+
+restart.addEventListener('click', clearBoard)
 
 
 //check the sqare marker
+function changeMarker() {
+    if(this.textContent == ''){
+        this.textContent = 'X'
+    }else if(this.textContent == 'X'){
+        this.textContent = 'O'
+    }else {
+        this.textContent = ''
+    }
+}
+
+for(var i=0; i<squares.length; i++){
+    squares[i].addEventListener('click', changeMarker);
+}
 
 //for loop to add event listeners to all the squares
