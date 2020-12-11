f5120f8976e4c1805b017a07affa54ea222fc97f
sys-ryan <sys.ryan0902@gmail.com>
2020-12-11 14:40:13 +0900

    Create 12. Django Level One

--- 12. Django Level One - Basic/first_project/first_app/views.py
+++ 12. Django Level One - Basic/first_project/first_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic, Webpage, AccessRecord
+
+
 # Create your views here.
 
 def index(request):
+    wepbages_list = AccessRecord.objects.order_by('date')
+    date_dict = {'access_records': wepbages_list}
     my_dict = {'insert_me':"Hello I am from views.py AGAIN!"}
-    return render(request, 'first_app/index.html', context=my_dict)
+    return render(request, 'first_app/index.html', context=date_dict)
